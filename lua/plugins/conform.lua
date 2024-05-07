a3f8bb9d5c2437191ed37edbc8b1509e9ddd3802
islashu <benjaminchoo97@gmail.com>
2024-05-07 18:09:03 +0800

    Fixed prettier not working when formatting with conform + fixed not being able to disable copilot using Copilot disable
    Fixed formatting of conform: this is due to the error where overwriting
    of the conform config formatting, this has thus been move to another
    autocmd location.
    
    Copilot disable issue can be solved with the copilot version, just need
    to update this.
    
    Note that do not update bufferline as the recent update causes it to not
    be able to have multiple buffers.

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -48,21 +48,21 @@ return {
     }
     return opts
   end,
-  config = function()
-    require("conform").setup({
-      -- This is to add a <cmd>
-      vim.api.nvim_create_user_command("Format", function(args)
-        local range = nil
-        if args.count ~= -1 then
-          local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-          range = {
-            start = { args.line1, 0 },
-            ["end"] = { args.line2, end_line:len() },
-          }
-        end
-        require("conform").format({ async = true, lsp_fallback = true, range = range })
-      end, { range = true }),
-    })
-  end,
+  -- config = function()
+  --   require("conform").setup({
+  --     -- This is to add a <cmd>
+  --     vim.api.nvim_create_user_command("Format", function(args)
+  --       local range = nil
+  --       if args.count ~= -1 then
+  --         local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+  --         range = {
+  --           start = { args.line1, 0 },
+  --           ["end"] = { args.line2, end_line:len() },
+  --         }
+  --       end
+  --       require("conform").format({ async = true, lsp_fallback = true, range = range })
+  --     end, { range = true }),
+  --   })
+  -- end,
   keys = { { "<leader>cf", "<cmd>Format<cr>", desc = "Format" } },
 }
