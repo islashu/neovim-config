b7be92e3c69dc8667bbdb870c86d082436a25be3
islashu <benjaminchoo97@gmail.com>
2024-06-24 18:16:38 +0800

    Updated toggle term to accept go files to auto run

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -11,6 +11,7 @@ return {
       -- keymap to run python file in terminal
       vim.keymap.set("n", "<leader>tt", function()
         local current_file_uri = vim.api.nvim_buf_get_name(0)
+        local current_path_uri = vim.loop.cwd() .. "/"
         -- get file type
         local file_type = vim.api.nvim_buf_get_option(0, "filetype")
         -- clear
@@ -18,6 +19,12 @@ return {
           require("toggleterm").exec("clear")
           -- run python file in terminal
           require("toggleterm").exec("python3" .. " " .. current_file_uri)
+        end
+
+        if file_type == "go" then
+          require("toggleterm").exec("clear")
+          -- run python file in terminal
+          require("toggleterm").exec("go run " .. "'" .. current_path_uri  .. "main.go" .. "'")
         end
       end, { desc = "Run file in terminal" }),
     })
